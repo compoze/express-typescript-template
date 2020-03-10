904cf8cbcbeb1bd86a6a8910702be6dc993aec07
spawn-dev <spawn-dev>
2020-03-10 13:57:54 -0600

    add dynamic port for heroku

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,9 @@ import loggerMiddleware from './middleware/logger'
 
 import PostsController from './controllers/posts/posts.controller'
 
+const port = +process.env.PORT || 5000
 const app = new App({
-    port: 5000,
+    port: port,
     controllers: [
         new PostsController()
     ],
