c84ad4ab6ab5b522682f2f15a229d5c9a1d955f9
CodyTroopDev <cody.troop@elko.dev>
2021-03-15 13:34:49 -0500

    move dev dependencies and pass test script

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    roots: ['<rootDir>'],
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   }
