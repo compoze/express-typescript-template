7a85bed294e9787c1cad260c4f7843aeae2c8c58
CodyTroopDev <cody.troop@elko.dev>
2021-03-15 13:30:23 -0500

    remove batect and pug, update packages

--- .batect/database/create-structure.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'CREATE DATABASE myapp'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'myapp')\gexec
-
-SELECT 'CREATE DATABASE admin'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'admin')\gexec
