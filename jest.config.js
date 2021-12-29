4127127a5c7e72132236052e205684d8ba983cc3
Andrew Larsen <andrew.larse514@gmail.com>
2021-12-29 14:20:30 -0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    roots: ['<rootDir>'],
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  }
+  preset: "ts-jest",
+  testEnvironment: "node",
+  // These are the defaults for Jest in case we want to change them later:
+  roots: ["<rootDir>"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
